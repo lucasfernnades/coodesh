a9d08ca882195273d69946b61073d653685627d0
Lucas Fernandes <lucas.fernandes97@hotmail.com>
2024-04-18 10:57:13 -0300

    task3 resolved

--- myinterview/src/main/java/com/example/TASK3.java
+++ myinterview/src/main/java/com/example/TASK3.java
@@ -1,7 +1,8 @@
 package com.example;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.HashSet;
+import java.util.Random;
 
 /**
  * Write a list and add an aleatory number of Strings. In the end, print out how
@@ -10,5 +11,32 @@ import java.util.Iterator;
  */
 public class TASK3 {
 
-  
+    private static final Random random = new Random();
+
+    public static void main(String[] args) {
+        ArrayList<String> listStrings = new ArrayList<String>();
+
+        int numberOfStrings = (random.nextInt(10));
+        for (int i = 0; i < numberOfStrings; i++) {
+            listStrings.add(randomString());
+        }
+
+        System.out.println("List of strings created:");
+        for (String str : listStrings) {
+            System.out.println(str);
+        }
+
+        HashSet<String> setListStrings = new HashSet<String>(listStrings);
+        System.out.println("\nQuantity of distinct items: " + setListStrings.size());
+    }
+
+    private static String randomString() {
+        StringBuilder sb = new StringBuilder();
+        int length = (random.nextInt(5));
+        for (int i = 0; i < length; i++) {
+            char randomChar = (char) ('a' + Math.random() * 26);
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
 }
